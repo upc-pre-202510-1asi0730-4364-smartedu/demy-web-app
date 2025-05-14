e1252e5add40d58870255147e6db830e70d32767
Kyrubi <u20221c486@upc.edu.pe>
2025-05-14 12:26:31 -0500

    feat(in-app-navigation): update in-app navigation routes.

--- router/index.js
+++ router/index.js
@@ -7,8 +7,11 @@ import MainLayout from "../src/shared/components/main-layout.component.vue";
 // Pages
 //import LoginPage from '../public/pages/login-page.vue'
 //import DashboardPage from '../workspace/pages/dashboard-page.vue'
+import Enrollment from "../src/enrollments/pages/enrollment-management.component.vue";
+import Student from "../src/enrollments/pages/student-management.component.vue"
 import Payment from "../src/billing/pages/payment.component.vue";
 import Organization from "../workspace/pages/organization.component.vue";
+import AcademicPeriod from "../src/enrollments/pages/academic-period-management.component.vue";
 //import ExpensesPage from '../finance/pages/expenses-page.vue'
 //import ReportsPage from '../finance/pages/reports-page.vue'
 
@@ -17,9 +20,13 @@ const routes = [
         path: '/',
         component: MainLayout,
         children: [
-            { path: 'organization', component: Organization },
+            { path: 'organization', component: Organization, children: [
+                    {path: 'academic-periods', component: AcademicPeriod}
+                ]},
             { path: 'payments', component: Payment },
-            { path: '', redirect: '/workspace' }
+            { path: 'enrollment', component: Enrollment},
+            { path: 'students', component: Student},
+            { path: '', redirect: '/organization' },
         ]
     }
 ]
