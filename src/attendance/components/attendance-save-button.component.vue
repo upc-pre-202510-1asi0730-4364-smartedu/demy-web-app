2e21d3b0ffaaaa09ac06c60d4ed562e110c53694
diegovilcasaboya <diegovilcatut@gmail.com>
2025-05-14 12:24:25 -0500

    feat: add attendance save button component

---
+++ src/attendance/components/attendance-save-button.component.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="save-button">
+    <pv-button
+        :label="$t('attendance-save')"
+        icon="pi pi-save"
+        severity="primary"
+        @click="$emit('saveClicked')"
+    />
+  </div>
+</template>
+
+<script>
+import Button from 'primevue/button'
+import {Button as PvButton} from "primevue";
+
+export default {
+  name: 'AttendanceSaveButton',
+  emits: ['click'],
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.save-button {
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
