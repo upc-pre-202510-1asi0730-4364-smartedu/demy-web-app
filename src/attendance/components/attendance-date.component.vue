84d26d64e988fb6e14f77bbcf6d9d6e9caec4f20
diegovilcasaboya <diegovilcatut@gmail.com>
2025-05-14 12:23:26 -0500

    feat: add attendance date component

---
+++ src/attendance/components/attendance-date.component.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="field">
+    <label for="date">{{$t('attendance-date')}}</label>
+    <Calendar
+        v-model="selectedDate"
+        dateFormat="dd/mm/yy"
+        showIcon
+        :manualInput="false"
+        placeholder="Selecciona una fecha"
+    />
+  </div>
+</template>
+
+<script>
+import Calendar from 'primevue/calendar'
+
+export default {
+  name: 'AttendanceDate',
+  components: { Calendar },
+  props: {
+    modelValue: Date
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    selectedDate: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.field {
+  margin-bottom: 1rem;
+}
+</style>
